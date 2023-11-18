28b18023e67933d6b8c0b4248e874edd7174892f
Richard Grönroos <richard.gronroos@forefront.se>
2023-11-18 18:02:34 +0100

    add conditional env variables

--- src/makeRequest.ts
+++ src/makeRequest.ts
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
 export const makeRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.VITE_API_URL
+    : import.meta.env.VITE_API_URL_DEV,
   headers: {
-    Authorization: 'Bearer ' + import.meta.env.VITE_API_TOKEN,
+    Authorization:
+      'Bearer ' + import.meta.env.PROD
+        ? import.meta.env.VITE_API_TOKEN_PROD
+        : import.meta.env.VITE_API_TOKEN,
   },
 })
